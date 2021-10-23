812fd9194685750d33a6397896557960bc89df0a
Yongkang He <he@yongkang.cloud>
2021-10-23 20:22:04 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -19,7 +19,7 @@ for i in `cat k10objects`;do echo $i;ibmcloud cos object-delete --bucket $MY_PRE
 echo "" | awk '{print $1}'
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
-echo "-------Total time for K10+Postgresql+Policy+OnDemandBackup is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
 echo "-------Created by Yongkang"
 echo "-------Email me if any suggestions or issues he@yongkang.cloud"
