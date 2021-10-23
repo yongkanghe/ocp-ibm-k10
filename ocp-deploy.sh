f80fb0aaf3be88bab15966b4564d4088fedce619
Yongkang He <he@yongkang.cloud>
2021-10-23 14:39:21 +0800

    Update ocp-deploy.sh

--- ocp-deploy.sh
+++ ocp-deploy.sh
@@ -1,14 +1,15 @@
 echo '-------Creating an OCP Cluster'
 starttime=$(date +%s)
 . setenv.sh
-
+MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+my_cos_instance_id=$(ibmcloud resource service-instance yhe-cos4yong1 --output json | jq '.[].id')
 #ibmcloud login --apikey @ibmapi.key
-ibmcloud target -g $MY_RESOURCE_GROUP
+ibmcloud target -g $MY_PREFIX-$MY_RESOURCE_GROUP
 ibmcloud oc cluster create vpc-gen2 \
-  --name $MY_CLUSTER --version $MY_OCP_VERSION_openshift \
+  --name $MY_PREFIX-$MY_CLUSTER --version $MY_OCP_VERSION_openshift \
   --zone $MY_ZONE --vpc-id $(cat my_vpc_id) \
   --subnet-id $(cat my_subnet_id) \
-  --cos-instance 'crn:v1:bluemix:public:cloud-object-storage:global:a/692d79f012494327bf4b5a9e8f915787:01b75142-4ef8-4b17-8e4d-4e9d38fc4ec3::' \
+  --cos-instance $my_cos_instance_id \
   --flavor $MY_WORKER_FLAVOR \
   --workers 4 #bx2.4x16, cx2.8x16
 
@@ -25,7 +26,7 @@ echo '-------Still Deploying OpenShift Cluster'
 sleep 300
 
 echo '-------Retrieving OpenShift Cluster kubeconfig'
-ibmcloud oc cluster config -c $MY_CLUSTER --admin
+ibmcloud oc cluster config -c $MY_PREFIX-$MY_CLUSTER --admin
 
 echo '-------Waiting for OCP Cluster up running'
 sleep 300
@@ -36,7 +37,7 @@ sleep 200
 #kubectl wait --for=condition=ready --timeout=900s -n openshift-console pod -l component=ui
 
 echo '-------Deploying OpenShift Container Storage' #Wait for at least 23 mins
-ibmcloud ks cluster addon enable openshift-data-foundation -c $MY_CLUSTER -f -y
+ibmcloud ks cluster addon enable openshift-data-foundation -c $MY_PREFIX-$MY_CLUSTER -f -y
 
 echo '-------Waiting for OCS services up running'
 sleep 360
