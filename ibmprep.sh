09458d0fd79745a0a03de94d5f33e2eb4f820663
Yongkang He <he@yongkang.cloud>
2021-10-23 14:21:05 +0800

    Update ibmprep.sh

--- ibmprep.sh
+++ ibmprep.sh
@@ -5,17 +5,23 @@ ibmcloud target -r $MY_REGION
 ibmcloud resource group-create $MY_PREFIX-$MY_RESOURCE_GROUP
 ibmcloud target -g $MY_PREFIX-$MY_RESOURCE_GROUP
 
-echo "-------Create a seprate VPC, Subnet, Public Gateway"
+echo "-------Create a new VPC"
 ibmcloud is vpc-create $MY_PREFIX-$MY_VPC
 ibmcloud oc vpcs --provider vpc-gen2 | grep $MY_PREFIX-$MY_VPC | awk '{print $2}' > my_vpc_id
+
+echo "-------Create a new Subnet"
 ibmcloud is vpc-address-prefixes $(cat my_vpc_id) | grep $MY_ZONE | awk '{print $3}' | sed -e 's/\/.*/\/24/g' > my_cidr_block
 ibmcloud is subnet-create $MY_PREFIX-$MY_SUBNET $(cat my_vpc_id) $MY_ZONE --ipv4-cidr-block $(cat my_cidr_block)
 ibmcloud is subnets | grep $MY_PREFIX-$MY_SUBNET | awk '{print $1}' > my_subnet_id
+
+echo "-------Create a new Public Gateway and associate to the Subnet"
 ibmcloud is public-gateway-create $MY_PREFIX-$MY_GATEWAY $(cat my_vpc_id) $MY_ZONE
 ibmcloud is subnet-update $MY_PREFIX-$MY_SUBNET --pgw $(ibmcloud is public-gateways | grep $MY_PREFIX-$MY_GATEWAY | awk '{print $1}')
 
 echo "-------Create an Cloud Object Storage instance"
 ibmcloud resource service-instance-create $MY_PREFIX-$MY_OBJECT_STORAGE cloud-object-storage standard global
+
+echo "-------Create an Cloud Object Storage Service Key"
 ibmcloud resource service-key-create $MY_PREFIX-$MY_SERVICE_KEY --instance-name $MY_PREFIX-$MY_OBJECT_STORAGE --parameters '{"HMAC":true}'
 ibmcloud resource service-key $MY_PREFIX-$MY_SERVICE_KEY --output JSON | jq '.[].credentials.cos_hmac_keys.access_key_id' > ibmaccess
 ibmcloud resource service-key $MY_PREFIX-$MY_SERVICE_KEY --output JSON | jq '.[].credentials.cos_hmac_keys.secret_access_key' >> ibmaccess
