c6b3d3a43bd8d55e4a69a539eeefa5f166193a7e
Yongkang He <he@yongkang.cloud>
2021-10-23 20:22:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+ocpk10starttime=$(date +%s)
+
 ./ocp-deploy.sh
 
 ./k10-deploy.sh
 
+ocpk10endtime=$(date +%s)
+ocpk10duration=$(( $ocpk10endtime - $ocpk10starttime ))
+echo "-------Total time is $(($ocpk10duration / 60)) minutes $(($ocpk10duration % 60)) seconds."
+echo "" | awk '{print $1}'
