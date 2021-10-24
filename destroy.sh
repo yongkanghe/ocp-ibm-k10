18a8d45dacee0f8f35f700064cc15e25c5480115
Yongkang He <he@yongkang.cloud>
2021-10-24 10:46:22 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -11,6 +11,11 @@ oc config delete-context $(oc config get-contexts -o name | grep $MY_CLUSTER | h
 
 ibmcloud oc cluster rm --cluster $MY_PREFIX-$MY_CLUSTER --force-delete-storage -f #-q
 
+echo '-------Deleting the bucket'
+ibmcloud cos objects --bucket $(cat my_ibm_bucket) --region $MY_REGION --output json| grep Key | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' > k10objects
+for i in `cat k10objects`;do echo $i;ibmcloud cos object-delete --bucket $(cat my_ibm_bucket) --key $i --region $MY_REGION --force;done 
+ibmcloud cos bucket-delete --bucket $(cat my_ibm_bucket) --region $MY_REGION --force
+
 echo "-------Clean up the resources Subnet, Gateway, Service Key, Storage Instance and Resource Group"
 ibmcloud is subnet-delete $MY_PREFIX-$MY_SUBNET -f
 ibmcloud is public-gateway-delete $MY_PREFIX-$MY_GATEWAY -f
@@ -19,11 +24,6 @@ ibmcloud resource service-key-delete $MY_PREFIX-$MY_SERVICE_KEY -g $MY_PREFIX-$M
 ibmcloud resource service-key-delete $(ibmcloud resource service-keys -q | grep -v Name | awk '{print $1}') -g $MY_PREFIX-$MY_RESOURCE_GROUP -f
 ibmcloud resource service-instance-delete $MY_PREFIX-$MY_OBJECT_STORAGE -g $MY_PREFIX-$MY_RESOURCE_GROUP -f
 ibmcloud resource group-delete $MY_PREFIX-$MY_RESOURCE_GROUP -f
-
-echo '-------Deleting the bucket'
-ibmcloud cos objects --bucket $(cat my_ibm_bucket) --region $MY_REGION --output json| grep Key | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' > k10objects
-for i in `cat k10objects`;do echo $i;ibmcloud cos object-delete --bucket $(cat my_ibm_bucket) --key $i --region $MY_REGION --force;done 
-ibmcloud cos bucket-delete --bucket $(cat my_ibm_bucket) --region $MY_REGION --force
 
 echo "" | awk '{print $1}'
 endtime=$(date +%s)
