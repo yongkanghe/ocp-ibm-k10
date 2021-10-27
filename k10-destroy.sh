51860616b0e11f34c08645d74c2217914b7be228
Yongkang He <he@yongkang.cloud>
2021-10-27 10:40:13 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -3,7 +3,7 @@ starttime=$(date +%s)
 MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
 
 echo "-------Delete mongodb & kasten-io"
-./helm uninstall postgres -n mongodb
+./helm uninstall mongodb -n mongodb
 ./helm uninstall k10 -n kasten-io
 kubectl delete ns mongodb
 kubectl delete ns kasten-io
